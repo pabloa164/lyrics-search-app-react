1adfe3021ab3f5e1d01857bf5fa49128e3df9524
pabloa164 <pabloa164@gmail.com>
2019-02-18 15:24:35 -0500

    Updates

---
+++ precache-manifest.6427103115be7ab61cb577dcb5bc7745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "212354cf605960f4b88b",
+    "url": "/lyrics-search-app-react/static/js/runtime~main.212354cf.js"
+  },
+  {
+    "revision": "3482a41aee5f27c14906",
+    "url": "/lyrics-search-app-react/static/js/main.3482a41a.chunk.js"
+  },
+  {
+    "revision": "f10db58ff52f8b3bd289",
+    "url": "/lyrics-search-app-react/static/js/2.f10db58f.chunk.js"
+  },
+  {
+    "revision": "3482a41aee5f27c14906",
+    "url": "/lyrics-search-app-react/static/css/main.a0804cc8.chunk.css"
+  },
+  {
+    "revision": "d4311cfd5c4a1b94da8a26dec42e1536",
+    "url": "/lyrics-search-app-react/index.html"
+  }
+];
